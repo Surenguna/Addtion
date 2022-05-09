ed0640102f72cffadc60f93185e2c475b985f57c
Surenguna <g.surendran007@gmail.com>
2022-05-09 21:24:49 +0530

    Build is Ready

---
+++ src/org/git/TestGit.java
@@ -0,0 +1,20 @@
+package org.git;
+
+public class TestGit {
+	
+	
+	public static void main(String[] args) {
+		
+		int a= 10;
+		int b= 20;
+		
+		int add = a+b;
+		int mul= a*b;
+		int div= b/a;
+		
+		System.out.println("The answer is:"+add);
+		System.out.println("The answer is :"+mul);
+		System.out.println("The answer is:"+div);
+	}
+
+}
